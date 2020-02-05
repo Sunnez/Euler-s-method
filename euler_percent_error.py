e8e07035a7b811099cd7294500774b99e7ce951d
Shun Oikawa <shun@Shuns-MacBook-Pro.local>
2020-02-04 21:08:13 -0800

    Improve comment readability

--- euler_percent_error.py
+++ euler_percent_error.py
@@ -2,21 +2,23 @@ import math
 
 print("Let's begin")
 
-
-def f(x,y): #return answer of equation
-    return 2*x*y #put f(x,y) here  #math.exp(-y)
+#----- CHANGE FROM HERE -----
+def f(x,y): #dev: return answer of equation
+    return 2*x*y #CHANGE: put function here  #for the equation e^(-y), type math.exp(-y) instead.
 
 def g(x): #return actual answer. To calc %error
-    return math.exp(x**2-1) #put F(x,y) here
+    return math.exp(x**2-1) #CHANGE: put F(x,y): integral of the original function f(x,y), here
 
-DELTA_X = 0.05  #delta x
-FINAL_X = 1.5  #ultimate value of x
+DELTA_X = 0.05  #CHANGE: delta x
+FINAL_X = 1.5  #CHANGE: final value of x
+x=1 #CHANGE: initial value of x
+y=1 #CHANGE: initial value of y
+#----- CHANGE UNTIL HERE -----
 
-x=1 #initial value of x
-y=1 #initial value of y
-a=0
-p=0
+a=0 #dev: valiable for an Actual value
+p=0 #dev: valiable for a Percent error
 
+#Arrays to print at last
 x_db = [x]
 y_db = [y]
 a_db = [a] #absolute error
@@ -24,7 +26,7 @@ p_db = [p] #% error
 
 print("Delta x =" + str(DELTA_X))
 
-#calculation
+#dev: calculation
 while x < FINAL_X:
     y = y + f(x,y)*DELTA_X #formula of the Euler's method
     x += DELTA_X
--- improved_euler.py
+++ improved_euler.py
@@ -2,16 +2,18 @@ import math
 
 print("Let's begin")
 
-
+#--- You need to change the number from here ---
 def f(x,y): #return answer of equation
-    return x+y #put f(x,y) here  #math.exp(-y)
+    return x+y #CHANGE: put a function f(x,y) here  #type "math.exp(-y)" instead of e**(-y)
 
-DELTA_X = 0.1  #delta x
-FINAL_X = 0.3  #ultimate value of x
+DELTA_X = 0.1  #CHANGE: delta x
+FINAL_X = 0.3  #CHANGE: final value of x
 
-x=0 #initial value of x
+x=0 #CHANGE: nitial value of x
+y=1 #CHANGE: initial value of y
 z=0 #predictor
-y=1 #initial value of y
+
+#--- until here -----------------------------
 
 
 x_db = [x]
@@ -21,8 +23,8 @@ y_db = [y]
 print("Delta x =" + str(DELTA_X))
 #calculation
 while x < FINAL_X:
-    z = y + DELTA_X*(x+y)
-    y = y + DELTA_X/2*((x+y)+(z+x+DELTA_X))
+    z = y + DELTA_X*(f(x,y))
+    y = y + DELTA_X/2*(f(x,y)+f(x,z))
     x += DELTA_X
 
     x_db.append(x)
